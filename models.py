08279e1ec7d561e0d25456ae9c512bd1e995cac8
test <sebastian.kuppa@web.de>
2023-01-26 12:29:33 +0800

    finished Card class, added card images to project folder

--- models.py
+++ models.py
@@ -2,5 +2,20 @@ import pygame
 import random
 from enum import Enum
 
+
+class Suits(Enum):
+    CLUB = 0
+    SPADE = 1
+    HEART = 2
+    DIAMOND = 3
+
+
 class Card:
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+        self.image = pygame.image.load('images/' + self.suit + '-' + str(self.value) + '.svg')
+
+
+class Deck:
     pass
