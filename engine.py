fd4841afc8b25dc48629a1f406a0d88b78d0e62b
test <sebastian.kuppa@web.de>
2023-01-27 13:48:24 +0800

    added GameEngine class

--- engine.py
+++ engine.py
@@ -8,3 +8,14 @@ class GameState(Enum):
     SNAPPING = 1
     ENDED = 2
 
+
+class GameEngine:
+    def __init__(self):
+        self.deck = Deck()
+        self.deck.shuffle()
+        self.player1 = Player(name='Player1', flipKey=pygame.K_q, snapKey=pygame.K_w)
+        self.player2 = Player(name='Player2', flipKey=pygame.K_o, snapKey=pygame.K_p)
+        self.pile = Pile()
+        self.deck.deal()
+        self.current_player = self.player1
+        self.state = GameState.PLAYING
