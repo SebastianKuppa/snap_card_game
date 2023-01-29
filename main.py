2c29bb9778fc3a2206e37c972997f3d6a5619293
test <sebastian.kuppa@web.de>
2023-01-29 13:57:22 +0800

    added game loop to main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import pygame
 
+import engine
+from models import *
+from engine import *
+
 
 pygame.init()
 bounds = (1024, 768)
 window = pygame.display.set_mode(bounds)
 pygame.display.set_caption('Snap Card Game')
 
+if __name__ == '__main__':
+    gameEngine = engine.GameEngine()
 
+    run = True
+    while run:
+        key = None
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
